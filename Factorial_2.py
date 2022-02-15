18a4c942f218ccca45575d43d136fe7a2b105b0f
petrOL97 <petrov.oleg.97@yandex.ru>
2022-02-15 15:03:04 +0300

    Update Factorial_2.py

--- Factorial_2.py
+++ Factorial_2.py
@@ -1,17 +1,14 @@
 def FactorialTailZerosCount(n):
-        if isinstance(n, int):
-                if n<0:
-                        return("You can't use negative number")
-                else:
-                        count = 0
-                        while(n > 0):
-                                n = n // 5
-                                count = count + n
-                        return(count)
+        if type(n) != int:
+                n=int(n)
+        if n<0:
+                return("You can't use negative number")
         else:
-                return('Invalid value')
-                
-
-print(FactorialTailZerosCount(int(input())))
+                count = 0
+                while(n > 0):
+                        n = n // 5
+                        count = count + n
+                return(count)
+print(FactorialTailZerosCount(input()))
 
     
