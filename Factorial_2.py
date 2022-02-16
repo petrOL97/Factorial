648b33166e3175968c5e4abbef72d678e6b6ee9f
petrOL97 <petrov.oleg.97@yandex.ru>
2022-02-16 21:39:40 +0300

    Update Factorial_2.py

--- Factorial_2.py
+++ Factorial_2.py
@@ -1,15 +1,14 @@
-def factorial_tail_zeros_count(n):
-''' Return count of zeros at the end of factorial's value'''
+def count_of_factorial_tail_zeros(n) -> int: # это тайп-хинтинг?
+    ''' Return count of zeros at the end of factorial's value'''
     if not isinstance(n, int):
         raise TypeError("Input value type must be INT")
-        factorial_tail_zeros_count(n)
+        count_of_factorial_tail_zeros(n)
     if n < 0:
         raise ValueError("You can't use negative number")
-    else:
-        count = 0
-        while(n > 0):
-            n = n // 5
-            count = count + n
-        return(count) # comment
+    count = 0
+    while n > 0:
+        n = n // 5
+        count += n
+    return count
 
-print(factorial_tail_zeros_count(input()))
+print(count_of_factorial_tail_zeros(input()))
